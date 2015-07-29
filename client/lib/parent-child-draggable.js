846e20537a312cb4c15f5b6fd4108afe7deef3b2
dbeeke <daniel@studiofonkel.nl>
2015-07-29 19:42:56 +0200

    header

--- client/lib/parent-child-draggable.js
+++ client/lib/parent-child-draggable.js
@@ -22,9 +22,9 @@ ParentChildsOverview.prototype.init = function(element) {
   _.each(this.groups, function (group) {
     output += '<ul class="group">'
 
-    output += '<div class="header">'
+    output += '<li class="header">'
     output += group.name
-    output += '</div>'
+    output += '</li>'
 
     _.each(group.children, function (child) {
       output += '<li class="child">'
@@ -42,21 +42,7 @@ ParentChildsOverview.prototype.init = function(element) {
   })
 
   .bind('sortupdate', function(e, ui) {
-      /*
 
-      This event is triggered when the user stopped sorting and the DOM position has changed.
-
-      ui.item contains the current dragged element.
-      ui.index contains the new index of the dragged element (considering only list items)
-      ui.oldindex contains the old index of the dragged element (considering only list items)
-      ui.elementIndex contains the new index of the dragged element (considering all items within sortable)
-      ui.oldElementIndex contains the old index of the dragged element (considering all items within sortable)
-      ui.startparent contains the element that the dragged item comes from
-      ui.endparent contains the element that the dragged item was added to (new parent)
-
-      */
-
-     console.log(ui)
   })
 }
 
